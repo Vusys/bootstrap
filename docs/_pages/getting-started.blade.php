922cb7dac143f8015a4cc2cdaf8b4f822e1b538e
Bryan <bryan@vuii.co.uk>
2025-09-14 15:14:16 +0100

    docs: fix broken asset paths and workflow triggers

--- docs/_pages/getting-started.blade.php
+++ docs/_pages/getting-started.blade.php
@@ -179,21 +179,21 @@
                 <ul class="thumbnails bootstrap-examples">
                     <li class="span3">
                         <a class="thumbnail" href="examples/starter-template.html">
-                            <img src="assets/img/examples/bootstrap-example-starter.png" alt="">
+                            <img src="media/img/examples/bootstrap-example-starter.png" alt="">
                         </a>
                         <h4>Starter template</h4>
                         <p>A barebones HTML document with all the Bootstrap CSS and JavaScript included.</p>
                     </li>
                     <li class="span3">
                         <a class="thumbnail" href="examples/hero.html">
-                            <img src="assets/img/examples/bootstrap-example-marketing.png" alt="">
+                            <img src="media/img/examples/bootstrap-example-marketing.png" alt="">
                         </a>
                         <h4>Basic marketing site</h4>
                         <p>Featuring a hero unit for a primary message and three supporting elements.</p>
                     </li>
                     <li class="span3">
                         <a class="thumbnail" href="examples/fluid.html">
-                            <img src="assets/img/examples/bootstrap-example-fluid.png" alt="">
+                            <img src="media/img/examples/bootstrap-example-fluid.png" alt="">
                         </a>
                         <h4>Fluid layout</h4>
                         <p>Uses our new responsive, fluid grid system to create a seamless liquid layout.</p>
@@ -201,21 +201,21 @@
 
                     <li class="span3">
                         <a class="thumbnail" href="examples/marketing-narrow.html">
-                            <img src="assets/img/examples/bootstrap-example-marketing-narrow.png" alt="">
+                            <img src="media/img/examples/bootstrap-example-marketing-narrow.png" alt="">
                         </a>
                         <h4>Narrow marketing</h4>
                         <p>Slim, lightweight marketing template for small projects or teams.</p>
                     </li>
                     <li class="span3">
                         <a class="thumbnail" href="examples/justified-nav.html">
-                            <img src="assets/img/examples/bootstrap-example-justified-nav.png" alt="">
+                            <img src="media/img/examples/bootstrap-example-justified-nav.png" alt="">
                         </a>
                         <h4>Justified nav</h4>
                         <p>Marketing page with equal-width navigation links in a modified navbar.</p>
                     </li>
                     <li class="span3">
                         <a class="thumbnail" href="examples/signin.html">
-                            <img src="assets/img/examples/bootstrap-example-signin.png" alt="">
+                            <img src="media/img/examples/bootstrap-example-signin.png" alt="">
                         </a>
                         <h4>Sign in</h4>
                         <p>Barebones sign in form with custom, larger form controls and a flexible layout.</p>
@@ -223,14 +223,14 @@
 
                     <li class="span3">
                         <a class="thumbnail" href="examples/sticky-footer.html">
-                            <img src="assets/img/examples/bootstrap-example-sticky-footer.png" alt="">
+                            <img src="media/img/examples/bootstrap-example-sticky-footer.png" alt="">
                         </a>
                         <h4>Sticky footer</h4>
                         <p>Pin a fixed-height footer to the bottom of the user's viewport.</p>
                     </li>
                     <li class="span3">
                         <a class="thumbnail" href="examples/carousel.html">
-                            <img src="assets/img/examples/bootstrap-example-carousel.png" alt="">
+                            <img src="media/img/examples/bootstrap-example-carousel.png" alt="">
                         </a>
                         <h4>Carousel jumbotron</h4>
                         <p>A more interactive riff on the basic marketing site featuring a prominent carousel.</p>
--- docs/resources/views/vendor/hyde/layouts/scripts.blade.php
+++ docs/resources/views/vendor/hyde/layouts/scripts.blade.php
@@ -22,11 +22,12 @@
 
 <script src="https://cdnjs.cloudflare.com/ajax/libs/jquery/1.9.1/jquery.min.js" integrity="sha512-jGR1T3dQerLCSm/IGEGbndPwzszJBlKQ5Br9vuB0Pw2iyxOy+7AK+lJcCC8eaXyz/9du+bkCy4HXxByhxkHf+w==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>
 <script src="{{ Asset::mediaLink('js/bootstrap.js') }}"></script>
-<script src="{{ Asset::mediaLink('js/application.js') }}"></script>
+
 
 {{--<script src="https://cdnjs.cloudflare.com/ajax/libs/holder/1.9/holder.min.js" integrity="sha512-6eV3XDgL/hvgNSJO5F1oSeUNa0Xp5Xbl74BwCQkfeyESKK97ZJW8LD3C6HNHfFz1ZvrT4wYtclNulQQ/5UKI3g==" crossorigin="anonymous" referrerpolicy="no-referrer"></script>--}}
-<script src="assets/js/google-code-prettify/prettify.js"></script>
+{{--<script src="assets/js/google-code-prettify/prettify.js"></script>--}}
 <script src="{{ Asset::mediaLink('js/google-code-prettify/prettify.js') }}"></script>
+<script src="{{ Asset::mediaLink('js/application.js') }}"></script>
 
 {{--<script src="assets/js/application.js"></script>--}}
 
